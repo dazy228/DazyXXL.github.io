c4144fb0153578bb2c4d997b378056108bda5851
Чупак Даниил Эдуардович <Cupa_bd63>
2019-12-15 11:17:08 +0200

    New

---
+++ js/Java.js
@@ -0,0 +1,98 @@
+var number = 55.5;
+alert(typeof number);
+alert(number + 20); //25.5
+var str ="25";
+var str1 ='25';
+alert (str);
+alert (typeof str);
+var strr = 23;
+alert (str + 10); // конкатенация
+number = "Hello";
+
+var nameNumber = 15;
+var b = nameNumber;
+alert(b);
+alert(nameNumber);
+nameNumber = 30;
+alert(nameNumber);
+var hello2 = "Hello";
+alert(hello2);
+
+var $ =1.5
+// объявили переменную сменем '$'
+var _ = $;
+// переменная с именем '_'
+alert($ + _); //3
+
+// Константы
+
+const COLOR_RED ="#ff050a";
+//COLOR_RED = 30;
+alert(COLOR_RED);
+
+var firstNum = 12, secondNum = 3;
+alert(firstNum + secondNum);
+alert(firstNum - secondNum);
+alert(firstNum * secondNum);
+alert(firstNum / secondNum);
+alert(firstNum % secondNum);
+
+var num = 10;
+alert(num++);
+alert(num);
+alert(num--);
+alert(num);
+
+var num1 = 10, num2 =5;
+document.write(text: (num1 > num2) + "<br>");
+document.write(text: (num1 < num2) + "<br>");
+document.write(text: (num1 >= num2) + "<br>");
+document.write(text: (num1 <= num2) + "<br>");
+document.write(text: (num1 == num2) + "<br>");
+document.write(text: (num1 != num2) + "<br>");
+
+var bool = true;
+if(num1 > num2) {
+    alert("Boolean is true");
+}else {
+    alert("Boolean is false");
+}
+bool ? alert("true") : alert("false");
+
+var years = prompt("Сколько вам лет?");
+alert(typeof years);
+alert('Вам ' + years + ' лет!');
+
+var num = "122";
+alert(typeof num);
+alert(num + 10);
+num = parseInt(num);
+alert(num);
+alert(typeof num);
+num = toString
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
